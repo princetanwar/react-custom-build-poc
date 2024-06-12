cb56df9c0f791fadc9f0ff1535c2e123a708cc9b
prince tanwar <ptanwar468@gmail.com>
2024-06-12 23:15:58 +0530

    update path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,8 +6,8 @@ import Home from "./components/Home";
 function App() {
   return (
     <Switch>
-      <Route path="/about" component={About} />
-      <Route path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/" component={Home} />
       <Route path="*" component={() => "404"} />
     </Switch>
   );
