f79169df5332b6d4ba640c8f03868faced7647d0
prince tanwar <ptanwar468@gmail.com>
2024-06-12 23:48:22 +0530

    use s3 regional domain name

--- aws/modules/s3/input_output.tf
+++ aws/modules/s3/input_output.tf
@@ -5,5 +5,5 @@ output "bucket_id" {
   value = aws_s3_bucket.my_storage_bucket.id
 }
 output "bucket_domain_name" {
-  value = aws_s3_bucket.my_storage_bucket.bucket_domain_name
+  value = aws_s3_bucket.my_storage_bucket.bucket_regional_domain_name
 }
